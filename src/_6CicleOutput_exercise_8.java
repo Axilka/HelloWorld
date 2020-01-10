8e191d911789858c50fc15558354908d8ebcb8d1
Valery <vaulin.cpp@gmail.com>
2020-01-11 02:49:55 +0300

    Отредактирован

--- src/_6CicleOutput_exercise_8.java
+++ src/_6CicleOutput_exercise_8.java
@@ -2,6 +2,6 @@ public class _6CicleOutput_exercise_8 {
     public static void main(String[] args)
     {
         for(int i=1; i<=50; i++)
-            System.out.printf("%d", i);
+            System.out.print(i);
     }
 }
