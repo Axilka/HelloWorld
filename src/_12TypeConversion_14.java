53c95931e71b4a15baab894ed53c886edbdad288
Valery <vaulin.cpp@gmail.com>
2020-01-11 03:03:41 +0300

    Создан класс с преобразованием типов

---
+++ src/_12TypeConversion_14.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class _12TypeConversion_14 {
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите число: ");
+
+        String S = in.nextLine();
+        System.out.println(S);
+
+        int X = Integer.parseInt(S);
+        System.out.println(X);
+
+        Double Y = (double) X;
+        System.out.println(Y);
+    }
+}
