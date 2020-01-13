84476e620bbc3f8dfb7a1d0bb8e77dc78419319a
Valery <vaulin.cpp@gmail.com>
2020-01-13 03:08:01 +0300

    Переименован

--- src/_12TypeConversion_exercise_14.java
+++ src/_12TypeConversion_exercise_14.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class _12TypeConversion_14 {
+public class _12TypeConversion_exercise_14 {
     public static void main(String[] args)
     {
         Scanner in = new Scanner(System.in);
